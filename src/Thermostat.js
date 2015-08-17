f1fd2228b4c35c2e04e5bff5d9de782a2c9a7da0
Leon Wee <leonwee27@gmail.com>
2015-08-17 18:29:38 +0700

    Finished thermostat starting JQuery

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,22 +1,27 @@
 var Thermostat = function Thermostat() {
   this.temperature = 20
   this.save_mode = 'on'
+  this.maximumOffTemperature = 25
+  this.maximumOnTemperature = 32
+  this.minimumTemperature = 11
+  this.greenTemperature = 18
+  this.orangeTemperature = 25
 };
 
 Thermostat.prototype.up = function() {
-  if (this.save_mode === 'on' && this.temperature === 25) {
-    return this.temperature = 25;
+  if (this.save_mode === 'on' && this.temperature === this.maximumOffTemperature) {
+    return this.temperature = this.maximumOffTemperature;
   }
 
-  if (this.save_mode === 'off' && this.temperature === 32) {
-    return this.temperature = 32;
+  if (this.save_mode === 'off' && this.temperature === this.maximumOnTemperature) {
+    return this.temperature = this.maximumOnTemperature;
   }
 
   return this.temperature += 1;
 };
 
 Thermostat.prototype.down = function() {
-  if (this.temperature < 11) {
+  if (this.temperature < this.minimumTemperature) {
     return 10;
   }
   return this.temperature -= 1;
@@ -38,11 +43,11 @@ Thermostat.prototype.reset_temperature = function() {
 
 Thermostat.prototype.colour = function() {
 
-  if (this.temperature < 18) {
+  if (this.temperature < this.greenTemperature) {
     return 'green';
   }
 
-  if (this.temperature < 25) {
+  if (this.temperature < this.orangeTemperature) {
     return 'orange';
   }
 
