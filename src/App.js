b7077e12a0b2119ccae875976b6f9a63a8fd253d
Leon Wee <leonwee27@gmail.com>
2015-08-17 18:51:32 +0700

    Changed the app

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
 var thermostat = new Thermostat();
 
-TemperatureColour = function() {
-  if (thermostat.temperature < 18) {
-    temperature.style.color = thermostat.colour();
-  }
+// TemperatureColour = function() {
+//   if (thermostat.temperature < 18) {
+//     temperature.style.color = thermostat.colour();
+//   }
 
-  else if (thermostat.temperature < 25) {
-    temperature.style.color = thermostat.colour();
-  }
+//   else if (thermostat.temperature < 25) {
+//     temperature.style.color = thermostat.colour();
+//   }
 
-  else {
-    temperature.style.color = thermostat.colour();
-  }
-}
+//   else {
+//     temperature.style.color = thermostat.colour();
+//   }
+// }
 
 var temperature = document.getElementById('temperature');
 temperature.innerHTML = thermostat.temperature;
-TemperatureColour();
+temperature.style.color = thermostat.colour();
 
 var up = document.getElementById('up');
 up.onclick = function up() {
   thermostat.up();
   temperature.innerHTML = thermostat.temperature;
-  TemperatureColour();
+  temperature.style.color = thermostat.colour();
 };
 
 var down = document.getElementById('down');
 down.onclick = function() {
   thermostat.down();
   temperature.innerHTML = thermostat.temperature;
-  TemperatureColour();
+  temperature.style.color = thermostat.colour();
 };
 
 var reset = document.getElementById('reset');
 reset.onclick = function() {
   thermostat.reset_temperature();
   temperature.innerHTML = thermostat.temperature;
-  TemperatureColour();
+  temperature.style.color = thermostat.colour();
 };
 
 var power_save = document.getElementById('mode')
