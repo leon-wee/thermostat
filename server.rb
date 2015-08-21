96e55cc0d9103e57743eef17cc46d11993c3bad7
Leon Wee <leonwee27@gmail.com>
2015-08-21 20:45:02 +0700

    Fixed persistence

--- server.rb
+++ server.rb
@@ -1,12 +1,24 @@
 require 'sinatra'
 require 'json'
 
+  enable :sessions
+  set :session_secret, 'secret'
   set :public_folder, proc { File.join(root) }
 
-get '/temperature/:remoteTemperature' do
-  session[:remoteTemperature] = params[:remoteTemperature]
+# get '/temperature/:remoteTemperature' do
+#   session[:remoteTemperature] = params[:remoteTemperature]
+# end
+#
+
+get '/temperature/:default' do
+  session[:persist_temperature] ||= params[:default]
+  { temperature: session[:persist_temperature] }.to_json
 end
 
+post '/temperature' do
+  session[:persist_temperature] = params[:current_temp]
+  p session
+end
 
 
 
--- src/App_Jquery.js
+++ src/App_Jquery.js
@@ -1,42 +1,84 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
 
+  var getTemp = function() {
+    $.getJSON("/temperature/" + thermostat.temperature, function(result) {
+      $('span').html(result.temperature);
+      thermostat.temperature = parseInt(result.temperature);
+      temperatureColour();
+    });
+  };
+
+
+  //{current_temp: thermostat.temperature});
+
+  var sendTemp = function() {
+    $.post('/temperature', {current_temp: thermostat.temperature});
+  };
+
+   // var getTemp = function() {
+   //   $.getJSON("/temperature" , function(result) {
+   //     $("span").text(result.temperature);
+   //       thermostat.temperature = parseInt(result.temperature);
+   //       temperatureColour();
+   //   });
+   // }
 
 
   function temperatureColour() {
     $('span').removeClass().addClass(thermostat.colour());
   };
 
-
   function update() {
     $('span').html(thermostat.temperature);
     temperatureColour();
   };
 
-  update();
+
+
+  getTemp();
+  // sendTemp();
+  // temperatureColour();
+
+
+  // function update() {
+  //   sendTemp();
+  //   getTemp();
+  // };
+
+  // function displaysTemperature() {
+  //   $('span').html(thermostat.temperature);
+  // };
+
+  // update();
+
+  // getCurrentTemperature();
 
   $('.plus').click(function() {
     thermostat.up();
     update();
+    sendTemp();
   });
 
   $('.minus').click(function() {
     thermostat.down();
     update();
+    sendTemp();
   });
 
   $('.reset').click(function() {
     thermostat.reset_temperature();
     update();
+    sendTemp();
   });
 
   $('.power').click(function() {
     thermostat.change_save_mode();
     update();
+    sendTemp();
   });
 
   $(".getWeather").click(function() {
-
     var city = $("input:text").val();
     weatherInfo(city);
     displayCity(city);
@@ -70,6 +112,11 @@ $(document).ready(function() {
       $('input:text').val('');
     }
   });
+
+
+
+
+
 
   // function getCurrentTemperature() {
   //   $.getJSON("http://localhost:9292/temperature/" + thermostat.temperature, function(remoteTemperature) {
